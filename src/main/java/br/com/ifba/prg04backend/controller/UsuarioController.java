e4f7ce8d890ecebe095f7239bbbb5070efd7870a
Kaue OLiveira <kauemendz@gmail.com>
2026-05-21 00:14:47 -0300

    Atividade 13: Mapeamento Completo dos Métodos HTTp no Controller

--- src/main/java/br/com/ifba/prg04backend/controller/UsuarioController.java
+++ src/main/java/br/com/ifba/prg04backend/controller/UsuarioController.java
@@ -4,9 +4,10 @@ import br.com.ifba.prg04backend.service.UsuarioService;
 import br.com.ifba.prg04backend.model.Usuario;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController // Define esta classe como um Controller REST
 @RequestMapping("/usuarios") // Define a rota base da API
@@ -20,10 +21,43 @@ public class UsuarioController {
     }
 
     @PostMapping // Endpoint para criar usuário
-    public ResponseEntity<Usuario> criar(Usuario usuario) {
+    public ResponseEntity<Usuario> criar(@RequestBody Usuario usuario) {
 
         Usuario usuarioSave = usuarioService.criar(usuario);
         return ResponseEntity.status(HttpStatus.CREATED).body(usuarioSave);
+
+    }
+
+    @GetMapping// Endpoint para listar usuarios
+    public ResponseEntity<List<Usuario>> ListarTodos(){
+
+        return ResponseEntity.ok(usuarioService.ListarTodos());
+    }
+
+    @GetMapping("/{id}") // Endpoint para buscar por id
+    public ResponseEntity<Usuario> BuscarPorID(@PathVariable Long id){
+
+        Optional<Usuario> usuario = usuarioService.BuscarPorID(id);
+
+        return usuario.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+
+    }
+
+    @GetMapping("/{id}") //Deletar por id
+    public ResponseEntity<Void> Deletar(@PathVariable Long id) {
+
+        usuarioService.Deletar(id);
+
+        return ResponseEntity.noContent().build();
+
+    }
+
+    @PutMapping("/{id}") //Altualizar por id
+    public ResponseEntity<Usuario> Altualizar(@PathVariable Long id, @RequestBody Usuario usuario) {
+
+        return usuarioService.Altualizar(id, usuario)
+                .map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+
 
     }
 }
--- src/main/java/br/com/ifba/prg04backend/service/UsuarioService.java
+++ src/main/java/br/com/ifba/prg04backend/service/UsuarioService.java
@@ -3,7 +3,10 @@ package br.com.ifba.prg04backend.service;
 import br.com.ifba.prg04backend.model.Usuario;
 import org.springframework.stereotype.Service;
 import br.com.ifba.prg04backend.repository.UsuarioRepository;
+
 import javax.swing.*;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
@@ -14,18 +17,15 @@ public class UsuarioService {
         this.usuarioRepository = usuarioRepository;
     }
 
+    //Metodo Listar
     public Usuario criar(Usuario usuario) {
 
         try {
             if (usuario.getName() == null) {
                 throw new RuntimeException("Nome de Usuario é Obrigátorio");
-            }
-
-            else if(usuario.getEmail() == null) {
+            } else if (usuario.getEmail() == null) {
                 throw new RuntimeException("Email Obrigátorio");
-            }
-
-            else if (usuario.getPassword().length() < 6){
+            } else if (usuario.getPassword().length() < 6) {
                 throw new RuntimeException("Senha muito Curta");
 
             }
@@ -37,5 +37,47 @@ public class UsuarioService {
                     "Erro ao criar usuário: " + e.getMessage());
         }
 
+    }
+//Metodo Listar
+    public List<Usuario> ListarTodos() {
+
+        return usuarioRepository.findAll();
+    }
+
+    public Optional<Usuario> BuscarPorID(Long id) {
+
+        return usuarioRepository.findById(id);
+
+    }
+//Metodo Deletar
+    public Boolean Deletar(long id) {
+
+
+        try {
+            if (usuarioRepository.existsById(id)) {
+                throw new RuntimeException("Usuario não encontrado");
+            }
+
+            usuarioRepository.deleteById(id);
+            return true;
+
+        } catch (Exception e) {
+            return false;
+
+        }
+
+
+    }
+
+    //metodo Altualizar
+    public Optional<Usuario> Altualizar(Long id, Usuario usuario) {
+
+        return usuarioRepository.findById(id).map(usuarioExistente -> {
+            usuarioExistente.setName(usuario.getName());
+            usuarioExistente.setEmail(usuario.getEmail());
+            usuarioExistente.setPassword(usuario.getPassword());
+
+            return usuarioRepository.save(usuarioExistente);
+        });
     }
 }
